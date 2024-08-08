799e9901d02f0d7abe7bd216e3abb136d61354e0
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2024-08-08 19:04:04 +1200

    build: Update Vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: './docs',  // Outputs the build to the docs folder (necessary for gitbucket pages)
+    emptyOutDir: true,  // also necessary
+  }
 })
