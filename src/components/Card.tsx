739237cba9890c43b4548a59456882951308bd74
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2024-12-21 23:49:42 +1300

    build: Fix " Heading elements are not in a sequentially-descending order"

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -22,9 +22,9 @@ export default function Card({props, aspectRatio, className}: CardProps) {
                 <img src={path} alt="" className="w-full h-full object-cover" />
             </div>
             <div className="py-2">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
                     {title}
-                </h5>
+                </h1>
                 <p className="mb-3 font-normal text-gray-700">
                     {desc}
                 </p>
