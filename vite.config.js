58a1f8574461ef0a1c4295174bb9af372c7d04ed
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2024-12-22 21:42:39 +1300

    build: Update base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/fhcl/",    // Use this for when hosting via the github pages link (https://jamesrobionyrogers.github.io/fhcl)
+  base: "/fhcl/",    // Use this for when hosting via the github pages link (https://jamesrobionyrogers.github.io/fhcl)
   build: {
     outDir: './docs',  // Outputs the build to the docs folder (necessary for gitbucket pages)
     emptyOutDir: true,  // also necessary
